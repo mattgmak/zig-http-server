ff06b0f75bc036d39426b260e4083b1973f59da1
mattgmak <u3592095@connect.hku.hk>
2026-04-06 18:15:26 +0800

    Chapter 1 done

--- src/main.zig
+++ src/main.zig
@@ -1,71 +1,46 @@
 const std = @import("std");
 const Io = std.Io;
+const Dir = std.Io.Dir;
 
 const zig_http_server = @import("zig_http_server");
 
+// Doing https://www.boot.dev/courses/learn-http-protocol-golang but in zig
 pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
     // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
-
-    // Accessing command line arguments:
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
-    }
-
-    // In order to do I/O operations need an `Io` instance.
+    // const arena: std.mem.Allocator = init.arena.allocator();
     const io = init.io;
 
     // Stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
+    // var stdout_buffer: [1024]u8 = undefined;
+    // var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+    // const stdout_writer = &stdout_file_writer.interface;
+    // try stdout_writer.print("I hope I get the job!\n", .{});
+    // try stdout_writer.flush(); // Don't forget to flush!
 
-    try zig_http_server.printAnotherMessage(stdout_writer);
+    const current_dir = Dir.cwd();
+    const file = try std.Io.Dir.openFile(current_dir, io, "messages.txt", .{});
+    defer file.close(io);
 
-    try stdout_writer.flush(); // Don't forget to flush!
+    var file_read_buffer: [1024]u8 = undefined;
+    var f_reader = file.reader(io, &file_read_buffer);
+
+    const alloc = init.gpa;
+    while (try nextLine(&f_reader.interface, alloc)) |line| {
+        defer alloc.free(line);
+        std.debug.print("read: {s}\n", .{line});
+    }
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
-
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
-    };
+fn nextLine(reader: *Io.Reader, alloc: std.mem.Allocator) !?[]u8 {
+    var bytes: std.Io.Writer.Allocating = .init(alloc);
+    errdefer bytes.deinit();
+    const read_result = reader.streamDelimiter(&bytes.writer, '\n');
+    if (read_result == error.EndOfStream) {
+        return null;
+    }
+    _ = try reader.takeByte();
+    const line = try bytes.toOwnedSlice();
+    return line;
 }
