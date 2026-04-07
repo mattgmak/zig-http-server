bb5aa79ddee59a2bad4177ad3fe48ff18deb4242
mattgmak <u3592095@connect.hku.hk>
2026-04-07 16:33:58 +0800

    CH2 L1

--- src/main.zig
+++ src/main.zig
@@ -10,27 +10,24 @@ pub fn main(init: std.process.Init) !void {
     // const arena: std.mem.Allocator = init.arena.allocator();
     const io = init.io;
 
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    // var stdout_buffer: [1024]u8 = undefined;
-    // var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    // const stdout_writer = &stdout_file_writer.interface;
-    // try stdout_writer.print("I hope I get the job!\n", .{});
-    // try stdout_writer.flush(); // Don't forget to flush!
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+    const stdout_writer = &stdout_file_writer.interface;
 
-    const current_dir = Dir.cwd();
-    const file = try std.Io.Dir.openFile(current_dir, io, "messages.txt", .{});
-    defer file.close(io);
-
-    var file_read_buffer: [1024]u8 = undefined;
-    var f_reader = file.reader(io, &file_read_buffer);
-
-    const alloc = init.gpa;
-    while (try nextLine(&f_reader.interface, alloc)) |line| {
-        defer alloc.free(line);
-        std.debug.print("read: {s}\n", .{line});
+    const ip: Io.net.IpAddress = try .parseIp4("127.0.0.1", 42069);
+    var server = try ip.listen(io, .{});
+    defer server.deinit(io);
+    const stream = try server.accept(io);
+    defer stream.close(io);
+    try stdout_writer.print("user connected\n", .{});
+    var reader_buffer: [1024]u8 = undefined;
+    var reader = stream.reader(io, &reader_buffer);
+    while (try nextLine(&reader.interface, init.gpa)) |line| {
+        try stdout_writer.print("{s}\n", .{line});
+        init.gpa.free(line);
     }
+    try stdout_writer.print("user disconnected\n", .{});
+    try stdout_writer.flush();
 }
 
 fn nextLine(reader: *Io.Reader, alloc: std.mem.Allocator) !?[]u8 {
